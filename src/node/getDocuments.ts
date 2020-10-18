754aec227f165fa7aeaf9a1b06de42b05d0a8792
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-10-18 14:59:51 +0200

    Fix node.js request using incorrect module

--- src/node/getDocuments.ts
+++ src/node/getDocuments.ts
@@ -1,4 +1,4 @@
-import http from 'http'
+import https from 'https'
 import split from 'split2'
 import {SanityDocument} from '@sanity/types'
 import {StreamResult} from '../types'
@@ -10,7 +10,7 @@ export function getDocuments(
   token?: string
 ): Promise<SanityDocument[]> {
   const headers = token ? {Authorization: `Bearer ${token}`} : undefined
-  const options: http.RequestOptions = {
+  const options: https.RequestOptions = {
     hostname: `${projectId}.api.sanity.io`,
     path: `/v1/data/export/${dataset}`,
     protocol: 'https:',
@@ -18,32 +18,36 @@ export function getDocuments(
   }
 
   return new Promise((resolve, reject) => {
-    http.request(options, (response) => {
-      const chunks: Buffer[] = []
-      if (response.statusCode !== 200) {
+    https
+      .request(options, (response) => {
+        response.setEncoding('utf8')
+
+        const chunks: Buffer[] = []
+        if (response.statusCode !== 200) {
+          response
+            .on('data', (chunk: Buffer) => chunks.push(chunk))
+            .on('end', () => {
+              const body = JSON.parse(Buffer.concat(chunks).toString('utf8'))
+              reject(new Error(`Error streaming dataset: ${getError(body)}`))
+            })
+          return
+        }
+
+        const documents: SanityDocument[] = []
         response
-          .on('data', (chunk: Buffer) => chunks.push(chunk))
-          .on('end', () => {
-            const body = JSON.parse(Buffer.concat(chunks).toString('utf8'))
-            reject(new Error(`Error streaming dataset: ${getError(body)}`))
+          .pipe(split(JSON.parse))
+          .on('data', (doc: StreamResult) => {
+            if (isStreamError(doc)) {
+              reject(new Error(`Error streaming dataset: ${doc.error}`))
+              return
+            }
+
+            if (doc && isRelevantDocument(doc)) {
+              documents.push(doc)
+            }
           })
-        return
-      }
-
-      const documents: SanityDocument[] = []
-      response
-        .pipe(split(JSON.parse))
-        .on('data', (doc: StreamResult) => {
-          if (isStreamError(doc)) {
-            reject(new Error(`Error streaming dataset: ${doc.error}`))
-            return
-          }
-
-          if (doc && isRelevantDocument(doc)) {
-            documents.push(doc)
-          }
-        })
-        .on('end', () => resolve(documents))
-    })
+          .on('end', () => resolve(documents))
+      })
+      .end()
   })
 }
