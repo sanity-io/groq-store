f7419d566ac4f2bbdc6633015fb796d1323c55f4
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-10-18 15:09:12 +0200

    Remove unused test

--- test/ndjsonStreamer.test.ts
+++
@@ -1,8 +0,0 @@
-import {getJsonStream} from '../src/browser/ndjsonStreamer'
-import {projectId, dataset} from './config'
-
-describe('can stream', () => {
-  it('works', async () => {
-    expect(documents).toBeGreaterThan(0)
-  })
-})
