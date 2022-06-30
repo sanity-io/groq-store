59d9d3077434f2b9d9a46751840152f21905d48b
jfulse <jorgen.fulsebakke@gmail.com>
2022-06-30 15:22:13 +0200

    Allow authentication with token in browser

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,17 +1,16 @@
+import EventSource from 'eventsource'
 import {groqStore as groqStoreApi} from '../groqStore'
-import {Config, GroqStore} from '../types'
+import {Config, EnvImplementations, GroqStore} from '../types'
 import {getDocuments} from './getDocuments'
 import {assertEnvSupport} from './support'
 
 export function groqStore(config: Config): GroqStore {
   assertEnvSupport()
 
-  if (config.token) {
-    throw new Error('`token` option not currently supported in browser')
-  }
-
   return groqStoreApi(config, {
-    EventSource: window.EventSource,
+    // Need to use eventsource library to enable authentication with token (e.g. Firefox and Safari)
+    // Native EventSource implementation does not accept headers
+    EventSource: (EventSource as any) as EnvImplementations['EventSource'],
     getDocuments,
   })
 }
