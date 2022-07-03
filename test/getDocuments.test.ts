323bb62a2558e8566497505d5ae6733f936c0de2
Rune Botten <rbotten@gmail.com>
2022-07-03 20:20:10 +0200

    test: add token test

---
+++ test/getDocuments.test.ts
@@ -0,0 +1,31 @@
+import EventSource from 'eventsource'
+import {groqStore as groqStoreApi} from '../src/groqStore'
+import {EnvImplementations, Config} from '../src/types'
+import * as baseConfig from './config'
+
+describe('getDocuments', () => {
+  it('calls it with the configured token', async () => {
+
+    const config : Config = {
+      ...baseConfig,
+      token: 'my-token'
+    }
+
+    const getDocuments = jest.fn().mockResolvedValue([])
+
+    const store = groqStoreApi(
+      config,
+      {
+        EventSource: EventSource as any as EnvImplementations['EventSource'],
+        getDocuments
+      }
+    )
+
+    await store.query('*')
+    expect(getDocuments).toBeCalledWith(
+      expect.objectContaining({
+        token: 'my-token'
+      }),
+    );
+  })
+})
