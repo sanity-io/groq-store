f40cf9559e33f6a6f17762dcc77eaf5d9c05beda
David Dios <david.dios@loveholidays.com>
2023-02-20 18:32:57 +0000

    adding support for events, adding datasetLoaded and datasetChanged

---
+++ src/events.ts
@@ -0,0 +1,26 @@
+import {EventEmitter} from 'events'
+import {SanityDocument} from '@sanity/types'
+
+type Events = {
+  /**
+   * Emitted after the dataset was loaded
+   */
+  datasetLoaded: {
+    dataset: string
+    documents: SanityDocument[]
+  }
+
+  /**
+   * Emitted each time when the dataset changes. This happens when the dataset
+   * is initialised, and after mutations were applied through listeners.
+   */
+  datasetChanged: {
+    dataset: string
+    documents: SanityDocument[]
+  }
+}
+
+export interface TypedEventEmitter extends EventEmitter {
+  on<K extends keyof Events>(s: K, listener: (v: Events[K]) => void): this
+  emit<K extends keyof Events>(eventName: K, params: Events[K]): boolean
+}
