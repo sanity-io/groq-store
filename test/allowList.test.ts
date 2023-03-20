7e4040ba44aae2b72df7b576ee700eb789b7553d
Cody Olsen <stipsan@gmail.com>
2023-03-21 00:23:08 +0100

    chore: fix test

--- test/allowList.test.ts
+++ test/allowList.test.ts
@@ -18,7 +18,7 @@ describe(
 
     test('only allow product documents in store', async () => {
       expect(await store.query(groq`count(*[_type == "vendor"])`)).toEqual(0)
-      expect(await store.query(groq`count(*[_type == "product"])`)).toEqual(12)
+      expect(await store.query(groq`count(*[_type == "product"])`)).toEqual(11)
     })
   },
   {timeout: 30000}
